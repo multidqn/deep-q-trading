455ec0269afdc11f1e106b7b6db07842615ff616
Antonio Sanna <tonionon@live.it>
2018-07-24 17:53:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -38,6 +38,7 @@ dqn.compile(Adam(lr=1e-3), metrics=['mae'])
 
 
 dqn.load_weights("Q.weights")
+"""
 for i in range(5):
     policy.set_eps(0.5)
     dqn.fit(environment, nb_steps=2000, callbacks=[FileLogger("Episodes.json", interval=100)], visualize=False, verbose=0)
@@ -55,5 +56,5 @@ for i in range(5):
     policy.set_eps(0.007)
     dqn.fit(environment, nb_steps=200000, callbacks=[FileLogger("Episodes.json", interval=100)], visualize=False, verbose=0)
     dqn.save_weights("Q.weights", overwrite=True)
-
+"""
 dqn.test(testEnv, nb_episodes=671, verbose=0, visualize=False)
