c634b6c26a6eff672599f05a2257ca44c1799fa2
Antonio <tonionon@live.it>
2018-07-25 21:06:02 +0200

    No message

--- main.py
+++ main.py
@@ -37,27 +37,25 @@ target_model_update=1e-2, policy=policy, test_policy=policyTest)
 dqn.compile(Adam(lr=1e-3), metrics=['mae'])
 
 
-dqn.load_weights("Q.weights")
-
-for i in range(5):
+for i in range(12):
     
     print(i)
     
     policy.set_eps(0.5)
-    dqn.fit(environment, nb_steps=2000, callbacks=[FileLogger("Episodes.json", interval=100)], visualize=False, verbose=0)
+    dqn.fit(environment, nb_steps=2000, visualize=False, verbose=0)
     dqn.save_weights("Q.weights", overwrite=True)
 
 
     policy.set_eps(0.1)
-    dqn.fit(environment, nb_steps=20000, callbacks=[FileLogger("Episodes.json", interval=100)], visualize=False, verbose=0)
+    dqn.fit(environment, nb_steps=20000, visualize=False, verbose=0)
     dqn.save_weights("Q.weights", overwrite=True)
 
     policy.set_eps(0.07)
-    dqn.fit(environment, nb_steps=200000, callbacks=[FileLogger("Episodes.json", interval=100)], visualize=False, verbose=0)
+    dqn.fit(environment, nb_steps=200000, visualize=False, verbose=0)
     dqn.save_weights("Q.weights", overwrite=True)
     
     policy.set_eps(0.007)
-    dqn.fit(environment, nb_steps=200000, callbacks=[FileLogger("Episodes.json", interval=100)], visualize=False, verbose=0)
+    dqn.fit(environment, nb_steps=200000, visualize=False, verbose=0)
     dqn.save_weights("Q.weights", overwrite=True)
 
-dqn.test(testEnv, nb_episodes=671, verbose=0, visualize=False)
+#dqn.test(testEnv, nb_episodes=671, verbose=0, visualize=False)
