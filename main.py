9214182da8e63c8b295518552c35b53487c50b88
TonioMeepo <tonionon@live.it>
2019-04-03 09:25:35 +0200

    vai ghicco

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ bot.send_message(chat_id='@DeepQTrading', text="Esperimento Iniziato "+str(datet
 nb_actions = 3
 
 model = Sequential()
-model.add(Flatten(input_shape=(50,1,68)))
+model.add(Flatten(input_shape=(10,1,68)))
 model.add(Dense(256,activation='linear'))
 model.add(LeakyReLU(alpha=.001)) 
 model.add(Dense(512,activation='linear'))
