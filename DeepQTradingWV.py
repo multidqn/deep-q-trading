15a5d3443328894d7e885bb466fee08e79d4f5cd
TonioMeepo <tonionon@live.it>
2019-04-03 09:35:54 +0200

    nenno

--- DeepQTradingWV.py
+++ DeepQTradingWV.py
@@ -21,11 +21,11 @@ class DeepQTrading:
         self.explorations=explorations
         self.nbActions=nbActions
         self.model=model
-        self.memory = SequentialMemory(limit=10000, window_length=50)
+        """self.memory = SequentialMemory(limit=10000, window_length=50)
         self.agent = DQNAgent(model=self.model, policy=self.policy,  nb_actions=self.nbActions, memory=self.memory, nb_steps_warmup=400, 
                             target_model_update=1e-1, enable_double_dqn=False, enable_dueling_network=False)
         self.agent.compile(Adam(lr=1e-3), metrics=['mae'])
-        self.agent.save_weights("q.weights", overwrite=True)
+        self.agent.save_weights("q.weights", overwrite=True)"""
         self.currentStartingPoint = begin
         self.trainSize=trainSize
         self.validationSize=validationSize
@@ -90,9 +90,10 @@ class DeepQTrading:
             del(self.memory)
             del(self.agent)
             self.memory = SequentialMemory(limit=180, window_length=10)
-            self.agent = DQNAgent(model=self.model, policy=self.policy,  nb_actions=self.nbActions, memory=self.memory, nb_steps_warmup=0, target_model_update=1000, enable_dueling_network=True)
+            self.agent = DQNAgent(model=self.model, policy=self.policy,  nb_actions=self.nbActions, memory=self.memory, nb_steps_warmup=200, target_model_update=1000,
+                                    enable_double_dqn=True,enable_dueling_network=True)
             self.agent.compile(Adam(lr=1e-3), metrics=['mae'])
-            self.agent.load_weights("q.weights")
+            #self.agent.load_weights("q.weights")
 
             trainMinLimit=None
 
