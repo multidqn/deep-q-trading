d233d06bcd7120ed379552d6dea935c119251367
TonioMeepo <tonionon@live.it>
2019-04-18 11:56:32 +0200

    we

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-
-# import os
-# os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID";
-# os.environ["CUDA_VISIBLE_DEVICES"]="1";
+import os
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID";
+os.environ["CUDA_VISIBLE_DEVICES"]="1";
 
 
 from DeepQTradingWV import DeepQTrading
