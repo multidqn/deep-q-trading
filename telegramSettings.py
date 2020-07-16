200cf4cd96830d8baaf0c96a64fa1e228b7a9a83
Antonio Sanna <tonionon@live.it>
2020-07-16 09:38:46 +0200

    Update telegramSettings.py

--- telegramSettings.py
+++ telegramSettings.py
@@ -1,3 +1,3 @@
 #Data regarding the bot and destination used for messages
-telegramToken='864997856:AAFjYS9qw9Gd3L_AwYGBPdhE7w-SWxf-JjU'
-telegramChatID='@DeepQTrading'
+telegramToken='INSERT_BOT_TOKEN_HERE'
+telegramChatID='@InsertChatIdHere'
